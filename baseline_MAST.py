0755d07c3320110e0ce3c0786954b22e1e586638
lamnaouarrr <100404310+lamnaouarrr@users.noreply.github.com>
2025-05-05 21:07:03 +0800

    switching to MAST

--- baseline_MAST.py
+++ baseline_MAST.py
@@ -42,6 +42,7 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 from skimage.metrics import structural_similarity as ssim
 from transformers import TFViTModel, ViTConfig
+from tensorflow.keras import layers  # Add explicit import for layers module
 ########################################################################
 
 ########################################################################

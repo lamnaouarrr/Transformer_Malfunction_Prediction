de9a4b8dc66c815d1b8708cbaa7f9c1143b85946
lamnaouarrr <100404310+lamnaouarrr@users.noreply.github.com>
2025-05-07 19:06:31 +0800

    fix MAST

--- baseline_MAST.py
+++ baseline_MAST.py
@@ -1080,12 +1080,16 @@ def create_mast_model(input_shape, mast_params, transformer_params):
         # Reshape to sequence
         x = layers.Reshape((total_patches, embed_dim))(x)
     
-    # Add classification token ([CLS]) before positional embedding
-    cls_token = layers.Layer(name="cls_token")(  
-        tf.Variable(initial_value=tf.random.normal([1, 1, embed_dim]), trainable=True)
+    # Create a trainable [CLS] token variable matching embedding dtype
+    cls_var = tf.Variable(
+        initial_value=tf.random.normal([1, 1, embed_dim], dtype=x.dtype),
+        trainable=True,
+        name="cls_token_var"
     )
-    cls_tokens = tf.repeat(cls_token, repeats=batch_size, axis=0)
+    cls_tokens = tf.repeat(cls_var, repeats=batch_size, axis=0)  # (batch_size,1,embed_dim)
+    # Concatenate CLS tokens with patch embeddings
     x = tf.concat([cls_tokens, x], axis=1)
+
     # Static positional embeddings applied after CLS concatenation
     positions = tf.range(start=0, limit=seq_len, delta=1)
     pos_embedding_layer = layers.Embedding(input_dim=seq_len, output_dim=embed_dim, name="position_embedding")
